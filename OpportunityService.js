3ffa3922decc79cebe49b92d8a3b59645173f83a
Edison Mulgeci <em3635@g.rit.edu>
2025-10-31 18:37:19 +0100

    Create OpportunityService.js

---
+++ OpportunityService.js
@@ -0,0 +1,9 @@
+class OpportunityService {
+  constructor(opportunityRepository) {
+    this.opportunityRepository = opportunityRepository;
+  }
+
+  postOpportunity(org, title, interest, location, date) {}
+  editOpportunity(opportunity) {}
+  deleteOpportunity(opportunity) {}
+}
