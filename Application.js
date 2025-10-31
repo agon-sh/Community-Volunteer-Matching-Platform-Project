1b9acadd3f94bc6e769b593b622b63aa44b2bc1c
3r1n1 <98190329+3r1n1@users.noreply.github.com>
2025-10-31 18:21:04 +0100

    created code skeleton for applicatoin

---
+++ Application.js
@@ -0,0 +1,9 @@
+class Application {
+  constructor(volunteer, opportunity, status) {
+    this.volunteer = volunteer;
+    this.opportunity = opportunity;
+    this.status = status;
+  }
+
+  updateStatus(newStatus){}
+}
