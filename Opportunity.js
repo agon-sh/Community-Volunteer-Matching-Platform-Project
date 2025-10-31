ef89f4df86e7fd319e0791a73225605fc8744bc1
3r1n1 <98190329+3r1n1@users.noreply.github.com>
2025-10-31 18:16:51 +0100

    created the opportunity skeleton

---
+++ Opportunity.js
@@ -0,0 +1,9 @@
+class Opportunity {
+  constructor(title, organization, interest, location, date) {
+    this.title = title;
+    this.organization = organization;
+    this.interest = interest;
+    this.location = location;
+    this.date = date;
+  }
+}
